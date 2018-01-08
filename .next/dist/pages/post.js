e499b866d001db9fb542603ef54fda6d9f8e15e0
0101kvmt <0101kvmt@gmail.com>
2018-01-08 07:44:33 -0800

    link props

--- .next/dist/pages/post.js
+++ .next/dist/pages/post.js
@@ -14,7 +14,11 @@ var _Layout2 = _interopRequireDefault(_Layout);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (props) {
-  return _react2.default.createElement(_Layout2.default, null, _react2.default.createElement('h1', null, ' ', props.url.query.title, ' '), _react2.default.createElement('p', null, ' blog post content! '));
+var Content = function Content(props) {
+  return _react2.default.createElement('div', null, _react2.default.createElement('h1', null, ' ', props.url.query.title, ' '), _react2.default.createElement('p', null, ' this is the content of the page '));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Bvc3QuanMiXSwibmFtZXMiOlsiTGF5b3V0IiwicHJvcHMiLCJ1cmwiLCJxdWVyeSIsInRpdGxlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBTyxBQUFQLEFBQW1CLEFBQW5CLEFBRUE7Ozs7OztrQkFBZSxVQUFDLEFBQUQsT0FBVyxBQUN4QjtTQUNFLGdCQUFDLEFBQUQsZ0NBQ0Usc0JBQUEsY0FBQSxNQUFBLE1BQU0sV0FBTSxBQUFOLElBQVUsQUFBVixNQUFnQixBQUF0QixPQUFBLEFBREYsQUFFRSxzQkFBQSxjQUFBLEtBQUEsTUFBQSxBQUZGLEFBREYsQUFPRDtBQVJEIiwiZmlsZSI6InBvc3QuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2t2bm10cm4vRGVza3RvcC9Qcm9qZWN0cy9qN18yMDE4L3ByYWN0aWNlL3Bvc3QvcG9zdC1uZXh0In0=
+
+exports.default = function (props) {
+  return _react2.default.createElement(_Layout2.default, null, _react2.default.createElement(Content, { url: props.url }));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Bvc3QuanMiXSwibmFtZXMiOlsiTGF5b3V0IiwiQ29udGVudCIsInByb3BzIiwidXJsIiwicXVlcnkiLCJ0aXRsZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFBUCxBQUFtQixBQUFuQjs7Ozs7O0FBRUEsSUFBTSxVQUFVLFNBQVYsQUFBVSxRQUFDLEFBQUQsT0FBVyxBQUN6QjtTQUNFLGdCQUFBLGNBQUEsT0FDRSxzQkFBQSxjQUFBLE1BQUEsTUFBTSxXQUFNLEFBQU4sSUFBVSxBQUFWLE1BQWdCLEFBQXRCLE9BQUEsQUFERixBQUVFLHNCQUFBLGNBQUEsS0FBQSxNQUFBLEFBRkYsQUFERixBQU1EO0FBUEQsQUFRQTs7a0JBQWUsVUFBQyxBQUFELE9BQVcsQUFDeEI7U0FDRSxnQkFBQyxBQUFELGdDQUNFLG9DQUFDLEFBQUQsV0FBUyxLQUFLLE1BQU0sQUFBcEIsQUFERixBQURGLEFBTUQ7QUFQRCIsImZpbGUiOiJwb3N0LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9rdm5tdHJuL0Rlc2t0b3AvUHJvamVjdHMvajdfMjAxOC9wcmFjdGljZS9wb3N0L3Bvc3QtbmV4dCJ9
--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,17 @@
 import Layout from '../components/Layout.js'
 
+const Content = (props) => {
+  return (
+    <div>
+      <h1> {props.url.query.title} </h1>
+      <p> this is the content of the page </p>
+    </div>
+  )
+}
 export default (props) => {
   return(
     <Layout>
-      <h1> {props.url.query.title} </h1>
-      <p> blog post content! </p>
+      <Content url={props.url} />
     </Layout>
   )
 
